bd831d9d969cfdadb434fcedc153ddf32d6eb10e
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-02-02 18:46:07 +0530

    home, navbar components created

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+	return (
+		<nav className='navbar'>
+			<h1>Techyy Blog</h1>
+			<div className='link'>
+				<a href='/'>Home</a>
+				<a href='/create'>New Blog</a>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
