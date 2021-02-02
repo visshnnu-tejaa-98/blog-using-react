3e51e3fe56d1d1437be274fe2e9b860ad4f26ab2
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-02-02 19:10:20 +0530

    button added in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
 const Home = () => {
+	const clickHandler = () => {
+		console.log('Hello React');
+	};
 	return (
 		<div className='home'>
 			<h1>Home Page</h1>
+			<button onClick={clickHandler}>Click Me</button>
 		</div>
 	);
 };
