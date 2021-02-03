f40387951dfda40927049c94cb7cbe407d080838
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-02-03 19:53:02 +0530

    added content to dynamic roots

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,10 +1,20 @@
 import { useParams } from 'react-router-dom';
+import useFetch from './useFetch';
 
 const BlogDetails = () => {
 	const { id } = useParams();
+	const { data: blog, isLoading, error } = useFetch(`http://localhost:8000/blogs/${id}`);
 	return (
 		<div className='blog-details'>
-			<h1>Blog Details-{id}</h1>
+			{error && <h1>sorry, unable to fetch data</h1>}
+			{isLoading && <h1>Loading...</h1>}
+			{blog && (
+				<article>
+					<h2>{blog.title}</h2>
+					<small>{blog.author}</small>
+					<p>{blog.body}</p>
+				</article>
+			)}
 		</div>
 	);
 };
