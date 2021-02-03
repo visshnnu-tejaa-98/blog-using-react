aa4f21e92701e5eef52e519bbd0a00cc7429e554
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-02-03 16:49:15 +0530

    condidtional loading message added

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,21 @@ import BlogList from './BlogList';
 
 const Home = () => {
 	const [blogs, setBlogs] = useState(null);
-
+	const [loading, setLoading] = useState(true);
 	useEffect(() => {
 		fetch(` http://localhost:8000/blogs`)
 			.then((res) => res.json())
 			.then((data) => {
 				setBlogs(data);
+				setLoading(false);
 			});
 	}, []);
-	return <div className='home'>{blogs && <BlogList blogs={blogs} title='All Blogs' />}</div>;
+	return (
+		<div className='home'>
+			{loading && <h1 className='text-center'>Loading...</h1>}
+			{blogs && <BlogList blogs={blogs} title='All Blogs' />}
+		</div>
+	);
 };
 
 export default Home;
