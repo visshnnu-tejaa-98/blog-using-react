90075ff6d73b03fbeea4c8f879df639fcb2c2908
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-02-03 21:30:29 +0530

    delete posyt functionality added

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,9 +1,15 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import useFetch from './useFetch';
 
 const BlogDetails = () => {
 	const { id } = useParams();
+	const history = useHistory();
 	const { data: blog, isLoading, error } = useFetch(`http://localhost:8000/blogs/${id}`);
+	const handleDelete = () => {
+		fetch(`http://localhost:8000/blogs/${blog.id}`, {
+			method: 'DELETE',
+		}).then(() => history.push('/'));
+	};
 	return (
 		<div className='blog-details'>
 			{error && <h1>sorry, unable to fetch data</h1>}
@@ -13,6 +19,7 @@ const BlogDetails = () => {
 					<h2>{blog.title}</h2>
 					<small>{blog.author}</small>
 					<p>{blog.body}</p>
+					<button onClick={handleDelete}>Delete</button>
 				</article>
 			)}
 		</div>
