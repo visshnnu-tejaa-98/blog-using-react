ce7fa471d1660d4a4c1ac8f5a968efc074cbd02c
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-02-03 17:21:24 +0530

    condidtional error message is implemented

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,17 +4,28 @@ import BlogList from './BlogList';
 const Home = () => {
 	const [blogs, setBlogs] = useState(null);
 	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
 	useEffect(() => {
-		fetch(` http://localhost:8000/blogs`)
-			.then((res) => res.json())
+		fetch(`http://localhost:8000/blogs`)
+			.then((res) => {
+				if (!res.ok) {
+					throw Error('Unable to fetch the data from server:(');
+				}
+				return res.json();
+			})
 			.then((data) => {
 				setBlogs(data);
+				setLoading(false);
+			})
+			.catch((err) => {
+				setError(err.message);
 				setLoading(false);
 			});
 	}, []);
 	return (
 		<div className='home'>
-			{loading && <h1 className='text-center'>Loading...</h1>}
+			{error && <h2>{error}</h2>}
+			{loading && <h2>Loading...</h2>}
 			{blogs && <BlogList blogs={blogs} title='All Blogs' />}
 		</div>
 	);
