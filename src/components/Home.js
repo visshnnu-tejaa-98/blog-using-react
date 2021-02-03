a4869558cad2dec5ff50c40a3af46d7081ace81d
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-02-03 16:09:50 +0530

    fetch data from json server

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,21 +2,16 @@ import React, { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-	const [blogs, setBlogs] = useState([
-		{ title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-		{ title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-		{ title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
-	]);
-	const handleDelete = (index) => {
-		let filteredItems = blogs.filter((blog) => blog.id !== index);
-		setBlogs(filteredItems);
-	};
-	useEffect(() => {}, []);
-	return (
-		<div className='home'>
-			<BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete} />
-		</div>
-	);
+	const [blogs, setBlogs] = useState(null);
+
+	useEffect(() => {
+		fetch(` http://localhost:8000/blogs`)
+			.then((res) => res.json())
+			.then((data) => {
+				setBlogs(data);
+			});
+	}, []);
+	return <div className='home'>{blogs && <BlogList blogs={blogs} title='All Blogs' />}</div>;
 };
 
 export default Home;
