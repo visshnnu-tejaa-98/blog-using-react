e8969534b36676087e231bf8897dd04c248589f4
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-02-03 20:19:06 +0530

    added form validation

--- src/components/CreateBlog.js
+++ src/components/CreateBlog.js
@@ -1,7 +1,24 @@
+import { useState } from 'react';
 const CreateBlog = () => {
+	const [title, setTitle] = useState('');
+	const [body, setBody] = useState('');
+	const [author, setAuthor] = useState('');
 	return (
 		<div className='create'>
-			<h1>Add New Blog</h1>
+			<form action=''>
+				<label>Title:</label>
+				<input type='text' required value={title} onChange={(e) => setTitle(e.target.value)} />
+				<label>Author:</label>
+				<input type='text' required value={author} onChange={(e) => setAuthor(e.target.value)} />
+				<label>Content:</label>
+				<textarea
+					rows='10'
+					required
+					value={body}
+					onChange={(e) => setBody(e.target.value)}
+				></textarea>
+				<button>Add Blog</button>
+			</form>
 		</div>
 	);
 };
