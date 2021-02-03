739fdce76824da44f3970cedec22020e6dc10f69
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-02-03 15:00:45 +0530

    created seperated component for blog list

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import BlogList from './BlogList';
+
 const Home = () => {
 	const [blogs, setBlogs] = useState([
 		{ title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
@@ -8,12 +10,7 @@ const Home = () => {
 	console.log(blogs);
 	return (
 		<div className='home'>
-			{blogs.map((blog) => (
-				<div className='blog-preview' key={blog.id}>
-					<h2>{blog.title}</h2>
-					<p>Written by {blog.author}</p>
-				</div>
-			))}
+			<BlogList blogs={blogs} title='All Blogs' />
 		</div>
 	);
 };
